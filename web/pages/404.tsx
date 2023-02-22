0d96ead74bacc7356d0aa7660f5149f1f7488bee
DL Salo <salo@objectsonore.com>
2023-02-22 15:05:55 -0800

    404 page fix, added tod

---
+++ web/pages/404.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
+export default function Custom404() {
+  const router = useRouter()
+  useEffect(() => {
+    router.push("/")
+  }, [])
+
+  return null
+}
