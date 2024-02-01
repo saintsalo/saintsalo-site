e0d2d840cb8afca534a9e95d990e6d6dbd27b0d0
DL Salo <salo@objectsonore.com>
2024-02-01 10:23:35 -0800

    corrected contact page

--- web/app/contact/page.tsx
+++ web/app/contact/page.tsx
@@ -6,11 +6,13 @@ export default function Page() {
       <h1>Contact</h1>
       <div>
         email: <Link href="mailto:saintsalo@gmail.com">saintsalo@gmail.com</Link>
+      </div>
+      <div>
         Object Sonore (Label) email:{" "}
         <Link href="mailto:salo@objectsonore.com">salo@objectsonore.com</Link>
       </div>
       <div>
-        dm instagram:{" "}
+        dm/instagram:{" "}
         <Link href="https://www.instagram.com/saintsalo/" target="_blank">
           @saintsalo
         </Link>
